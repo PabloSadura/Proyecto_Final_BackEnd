28db77c5a7da756dc951c3c82cb857b0c2da2bff
PabloSadura <pablosadura@gmail.com>
2022-12-21 12:59:06 -0300

    cambio en el archivo config

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ export const config = {
   GOOGLE_KEY_ID: process.env.GOOGLE_KEY_ID,
   GOOGLE_SECRET_KEY: process.env.GOOGLE_SECRET_KEY,
   MONGO_URL: process.env.MONGO_URL,
-  PORT: process.argv[2] || 8081,
+  PORT: process.env.PORT || 8081,
   MODO: process.argv[3] || process.env.MODO,
 };
