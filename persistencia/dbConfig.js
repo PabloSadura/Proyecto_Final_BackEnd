afb3b50902acb5353d1e4783b6e5a83541bda4ff
PabloSadura <pablosadura@gmail.com>
2023-01-21 16:11:30 -0300

    Se reinicio el proyecto a cero

--- persistencia/dbConfig.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export default async function dbConnect() {
-  const URL =
-    "mongodb+srv://ecommerce:coderhouse@cluster0.qrpyisw.mongodb.net/ecommerce?retryWrites=true&w=majority";
-
-  mongoose.connect(URL);
-}
--- routes/info.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const infoRouter = Router();
-
-infoRouter.get("/", (req, res) => {
-  res.render("info", process);
-});
-
-export default infoRouter;
